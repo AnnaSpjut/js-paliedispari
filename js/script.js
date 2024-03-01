47a6ab98803264f27051778d9b4e29ba4a1c4409
Anna Spjut <anna.spjut@gmail.com>
2024-03-01 15:47:51 +0400

    [remove] comment pari e dispari

--- js/script.js
+++ js/script.js
@@ -22,25 +22,25 @@ if(palindroma(parola)){
 
 // PARI E DISPARI
 
-// const pariDispari = prompt('Scegli e scrivi pari o dispari')
-// const numUser = prompt('Inserisci un numero tra 1 e 5')
-// const numComputer = getRandomNumber('min, max');
-// const somma = (parseInt(numUser) + parseInt(numComputer));
+const pariDispari = prompt('Scegli e scrivi pari o dispari')
+const numUser = prompt('Inserisci un numero tra 1 e 5')
+const numComputer = getRandomNumber('min, max');
+const somma = (parseInt(numUser) + parseInt(numComputer));
 
-// function getRandomNumber(min, max){
-//   return Math.floor(Math.random() *5 + 1); 
-// }
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() *5 + 1); 
+}
 
-// if(somma % 2 === 0){
-//   result = 'pari'
-// }else{
-//   result = 'dispari';
+if(somma % 2 === 0){
+  result = 'pari'
+}else{
+  result = 'dispari';
 
-//   if(result === pariDispari){
-//     finalResult = 'Hai vinto!'
-//   }else{
-//     finalResult = 'Mi dispiace, ha vinto il computer!'
-//   }
-// }
+  if(result === pariDispari){
+    finalResult = 'Hai vinto!'
+  }else{
+    finalResult = 'Mi dispiace, ha vinto il computer!'
+  }
+}
 
-// output2.innerHTML = finalResult;
+output2.innerHTML = finalResult;
